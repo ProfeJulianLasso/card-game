5f9896c1c1e801be78fbf1b16b68889cc2c8d69c
Julian Lasso <julian.lasso@sofka.com.co>
2023-03-31 09:26:41 -0500

    instalación de angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './modules/main/app-routing.module';
 import { AppComponent } from './modules/main/layout/app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
